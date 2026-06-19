4551e59d291bea599f5f8639322642d9cb2d3344
Anand <umeshanand324@gmail.com>
2026-06-19 23:46:32 +0530

    done

--- inspirit-jsx/src/components/site/IntroLoader.jsx
+++ inspirit-jsx/src/components/site/IntroLoader.jsx
@@ -33,7 +33,7 @@
 
 import { useEffect, useRef, useState } from 'react';
 import { gsap } from 'gsap';
-import logo from '@/assets/inspirit-logo.jpeg';
+import logo from '@/assets/inspirit-logo.png'
 export default function IntroLoader() {
     const [done, setDone] = useState(false);
     const root = useRef(null);
