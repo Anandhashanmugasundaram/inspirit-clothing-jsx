b9d04acfdbeb81f7e88cfdf3b9a8a37a8aefd667
Haribaskar <baskarh54@gmail.com>
2026-07-02 18:13:29 +0530

    testing

--- inspirit-jsx/src/App.jsx
+++ inspirit-jsx/src/App.jsx
@@ -72,7 +72,7 @@ export default function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} /> */}
           <Route path="/offers" element={<Offers />} />
-          {/* <Route path="/account" element={<Account />} /> */}
+          <Route path="/account" element={<Account />} />
           <Route path="/admin" element={<AdminPage />} />
           <Route path="/register" element={<Register />} />
           <Route path="/forgot" element={<Forgot />} />
