f7846f35caba46436cd8e536756942eae59054a3
Anand <umeshanand324@gmail.com>
2026-08-13 19:13:14 +0530

    size error done

--- inspirit-jsx/backend/server.js
+++ inspirit-jsx/backend/server.js
@@ -70,10 +70,6 @@ app.use((err, req, res, next) => {
 });
 
 // ==========================
-// PORT
+// VERCEL
 // ==========================
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
+module.exports = app;
