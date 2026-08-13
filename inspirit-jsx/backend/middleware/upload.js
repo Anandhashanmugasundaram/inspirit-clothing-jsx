953992b60de069eda9edc6722cfd2f055804b58b
Anand <umeshanand324@gmail.com>
2026-08-13 19:37:10 +0530

    mb changed

--- inspirit-jsx/backend/middleware/upload.js
+++ inspirit-jsx/backend/middleware/upload.js
@@ -15,7 +15,7 @@ const storage = new CloudinaryStorage({
 const upload = multer({
   storage,
   limits: {
-    fileSize: 5 * 1024 * 1024, // 5MB limit (important for Render)
+    fileSize: 10 * 1024 * 1024, // 5MB limit (important for Render)
   },
 });
 
